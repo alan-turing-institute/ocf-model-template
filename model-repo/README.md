6005473d1b543baa36e8d7939f113fcfb0b9ebe8
Nathan Simpson <nsimpson@turing.ac.uk>
2024-09-25 14:54:19 +0100

    add baskerville script

--- model-repo/README.md
+++ model-repo/README.md
@@ -2,3 +2,13 @@
 
 This is an implementation of the {{ model_name }} model for the work on cloud forecasting with [Open Climate Fix](https://openclimatefix.org/).
 It uses [`cloudcasting`](https://github.com/alan-turing-institute/cloudcasting) to validate models against EUMETSAT data.
+
+## Usage
+
+```bash
+pip install -e .
+```
+
+```bash
+cloudcasting validate
+```
---
+++ model-repo/validate-baskerville.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --qos turing
+#SBATCH --account=vjgo8416-climate
+#SBATCH --nodes 1
+#SBATCH --gpus-per-task 1
+#SBATCH --tasks-per-node 1
+#SBATCH --gpus 1
+#SBATCH --cpus-per-gpu 36
+#SBATCH --time 45:00:00
+#SBATCH --job-name {{ model_name }}-validation
+
+# drop into baskerville
+module purge
+module restore system
+module load bask-apps/live
+module load Python/3.11.3-GCCcore-12.3.0
+cd /bask/projects/v/vjgo8416-climate/shared/
+# check if repo exists
+if [ ! -d "{{ repo_name }}" ]; then
+    echo "Repo does not exist; cloning..."
+    git clone https://github.com/alan-turing-institute/{{ repo_name }}.git
+fi
+cd {{ repo_name }}
+
+# create virtual environment if it doesn't exist
+if [ ! -d ".venv" ]; then
+    echo "Virtual environment does not exist; creating..."
+    python -m venv .venv
+fi
+source .venv/bin/activate
+pip install -e .
+# upgrade jax to run metrics on GPU
+pip install --upgrade "jax[cuda12]"
+
+# run validation
+cloudcasting validate
